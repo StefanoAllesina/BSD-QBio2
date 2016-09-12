497ea95859f86176c3a670ce95eb62f0ec4de2b6
Sylvia Ranjeva <slr@uchicago.edu>
2016-09-12 10:05:13 -0500

    PDF plotting

--- workshops/cobey/code/2str_simulation.R
+++ workshops/cobey/code/2str_simulation.R
@@ -135,8 +135,8 @@ output <- data.frame(time = time,
 # Plotting #############################################################
 
 ## plot time-series #####################################################
-filename = "time_series.png"
-png(filename)
+filename = "time_series.pdf"
+pdf(filename)
 
 # plotting parameters
 par(mfrow = c(3, 1),     # 3x1 layout
@@ -161,8 +161,8 @@ plot(time, I2, ylim = c(0,.005), type = 'l', col = "red", lwd =1 , xlab = "Time"
 dev.off()
 
 ## plot I1 vs I2 #####################################################
-filename = "I1_vs_I2.png"
-png(filename)
+filename = "I1_vs_I2.pdf"
+pdf(filename)
 par(mfrow=c(1,1))
 plot(I1, I2, type = 'l', col = "red", lwd =1 , xlab = expression('I'[1]), ylab= expression('I'[2]), main = "Prevalence Strain 2 vs. Prevalence Strain 1")
 dev.off()
